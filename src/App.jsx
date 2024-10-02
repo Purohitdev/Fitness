518bf26ef9f75f19cf52e7af5661bab66f896ac7
dev <166125538+Purohitdev@users.noreply.github.com>
2024-10-02 14:26:09 +0530

    Revert "new routes"
    This reverts commit 72593f4ffe29adbef2142d3d698b5d67b89320e8.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,4 @@
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import './App.css';
-// import Home from './Comp/Home/Home';
-// import About from './Comp/About/About';
-// import Testimonials from './Comp/Testimonials/Testimonials';
-// import Trainings from './Comp/Trainings/Trainings';
-// import Contact from './Comp/Contact/Contact';
-// import Navb from './Comp/Navb';
-// import Foot from './Comp/Foot';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Navb />
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/trainings' element={<Trainings />} />
-//         <Route path='/testimonials' element={<Testimonials />} />
-//         <Route path='/contact' element={<Contact />} />
-//       </Routes>
-//       <Foot />
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Comp/Home/Home';
 import About from './Comp/About/About';
@@ -42,14 +10,8 @@ import Foot from './Comp/Foot';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.location.reload();
-  }, [location.pathname]);
-
   return (
-    <>
+    <Router>
       <Navb />
       <Routes>
         <Route path='/' element={<Home />} />
@@ -59,18 +21,9 @@ function App() {
         <Route path='/contact' element={<Contact />} />
       </Routes>
       <Foot />
-    </>
-  );
-}
-
-function AppWrapper() {
-  return (
-    <Router>
-      <App />
     </Router>
   );
 }
 
-export default AppWrapper;
-
+export default App;
 
